68204d906a14292935c0e6be0924bd14538fa913
Krittinuns <krittinun.sirodom@gmail.com>
2019-10-25 17:59:10 +0700

    [initial]

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import StackedBar from './component/chart/StackedBar'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        ApexChart
+
+        <StackedBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/component/chart/StackedBar.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+
+class StackedBar extends Component {
+
+  MOCK_ITERATION = 10
+  
+  mockCategory = () => {
+    let categories = []
+    for (let i=1 ; i<=this.MOCK_ITERATION ; i++)
+      categories.push(`${i}`)
+    return categories
+  }
+
+  mockProduct = () => {
+    let products = []
+    for (let i=1 ; i<=this.MOCK_ITERATION ; i++){
+      const value = Math.floor(Math.random() * 10) + 1;
+      products.push(value)
+    }
+    return products
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: {
+        chart: {
+          stacked: true,
+          toolbar: {
+            show: true
+          },
+          zoom: {
+            enabled: true,
+            type: "y"
+          }
+        },
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            legend: {
+              position: 'bottom',
+              offsetX: -10,
+              offsetY: 0
+            }
+          }
+        }],
+        plotOptions: {
+          bar: {
+            horizontal: false,
+          },
+        },
+
+        xaxis: {
+          type: 'category',
+          categories: this.mockCategory(),
+        },
+        legend: {
+          position: 'right',
+          offsetY: 40
+        },
+        fill: {
+          opacity: 1
+        }
+      },
+      series: [{
+        name: 'PRODUCT A',
+        data: this.mockProduct() // [44, 55, 41, 67, 22, 43]
+      }, {
+        name: 'PRODUCT B',
+        data: this.mockProduct() // [13, 23, 20, 8, 13, 27]
+      }, {
+        name: 'PRODUCT C',
+        data: this.mockProduct() // [11, 17, 15, 15, 21, 14]
+      }, {
+        name: 'PRODUCT D',
+        data: this.mockProduct() // [21, 7, 25, 13, 22, 8]
+      }],
+    }
+  }
+
+  render() {
+    return (
+      <div className="blahbla-app">
+        <div className="haha-row">
+          <div className="really-mixed-chart">
+            <Chart
+              options={this.state.options}
+              series={this.state.series}
+              type="bar"
+              height="300px"
+              width="500"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default StackedBar;
